179682673b8a50824abd7fb3e8642c617bbbb698
gshahbazian <gabeshahbazian@gmail.com>
2025-02-15 18:23:29 -0800

    Icon and middleware tweaks

--- src/middleware.ts
+++ src/middleware.ts
@@ -19,6 +19,6 @@ export const config = {
      * - _next/image (image optimization files)
      * - favicon.ico, sitemap.xml, robots.txt (metadata files)
      */
-    '/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)',
+    '/((?!api|_next/static|_next/image|favicon.ico|icon.png|apple-icon.png|sitemap.xml|robots.txt).*)',
   ],
 }
