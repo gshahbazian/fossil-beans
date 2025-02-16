02791d131ed68c687f7405fd7bd97bd7e61de053
gshahbazian <gabeshahbazian@gmail.com>
2025-02-15 17:04:11 -0800

    Even better icons?

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,6 @@ import { type Metadata } from 'next'
 
 export const metadata: Metadata = {
   title: 'Fossil Beans',
-  icons: [{ rel: 'icon', url: '/favicon.ico' }],
 }
 
 const giest = Geist({ subsets: ['latin'] })
