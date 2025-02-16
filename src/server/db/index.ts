e8ff94a64e90f0646f66099f64acfaef8b43d2c4
gshahbazian <gabeshahbazian@gmail.com>
2025-02-16 15:05:56 -0800

    Use neon serverless connection

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,18 +1,25 @@
-import { drizzle } from 'drizzle-orm/postgres-js'
-import postgres from 'postgres'
-
 import { env } from '@/env'
 import * as schema from './schema'
+import { neon, neonConfig } from '@neondatabase/serverless'
+import { drizzle } from 'drizzle-orm/neon-http'
 
 /**
- * Cache the database connection in development. This avoids creating a new connection on every HMR
- * update.
+ * fossil-beans uses neon serverless db for postgres
+ *
+ * Docs for connecting drizzle to neon:
+ * https://orm.drizzle.team/docs/connect-neon
+ * https://neon.tech/docs/serverless/serverless-driver
  */
-const globalForDb = globalThis as unknown as {
-  conn: postgres.Sql | undefined
+
+const connectionString = env.DATABASE_URL
+
+if (env.NODE_ENV === 'development') {
+  neonConfig.fetchEndpoint = (host) => {
+    const [protocol, port] =
+      host === 'db.localtest.me' ? ['http', 4444] : ['https', 443]
+    return `${protocol}://${host}:${port}/sql`
+  }
 }
 
-const conn = globalForDb.conn ?? postgres(env.DATABASE_URL)
-if (env.NODE_ENV !== 'production') globalForDb.conn = conn
-
-export const db = drizzle(conn, { schema, casing: 'snake_case' })
+const sql = neon(connectionString)
+export const db = drizzle({ client: sql, schema, casing: 'snake_case' })
