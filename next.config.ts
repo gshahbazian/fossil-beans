9eb78d4acc8f7c007bdd7c0056f47d36726d8eb1
gshahbazian <gabeshahbazian@gmail.com>
2025-02-17 08:15:51 -0800

    ts env and config

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,12 @@
+import type { NextConfig } from 'next'
+
 /**
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
-import './src/env.js'
+import './src/env.ts'
 
-/** @type {import("next").NextConfig} */
-const config = {
+const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
@@ -16,4 +17,4 @@ const config = {
   },
 }
 
-export default config
+export default nextConfig
