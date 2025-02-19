826fd2d3c53a6d832550919ff3334340e1d08a31
gshahbazian <gabeshahbazian@gmail.com>
2025-02-19 10:44:57 -0800

    rm group

--- src/components/game-stats.tsx
+++ src/components/game-stats.tsx
@@ -52,7 +52,7 @@ export default function GameStats({
 
         <div
           ref={headerRef}
-          className="group bg-background border-background container-grid sticky -top-0.5 z-2 col-span-full data-stuck:border-b data-stuck:shadow-sm sm:relative sm:top-auto sm:z-auto dark:border-neutral-900"
+          className="bg-background border-background container-grid sticky -top-0.5 z-2 col-span-full data-stuck:border-b data-stuck:shadow-sm sm:relative sm:top-auto sm:z-auto dark:border-neutral-900"
         >
           <h2 className="col-[2] text-2xl font-bold">
             {game.awayTeam.abbreviation}{' '}
