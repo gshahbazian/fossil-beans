94a7a88929381ec54886991f0a03cd71508f3a2b
gshahbazian <gabeshahbazian@gmail.com>
2025-10-22 09:56:45 -0700

    grid cols cleanup

--- src/components/games-page.tsx
+++ src/components/games-page.tsx
@@ -14,7 +14,7 @@ export default async function GamesPage({
   games: GameWithTeams[]
 }) {
   return (
-    <main className="grid w-screen grid-cols-[1fr_min(56rem,100%_-_1rem)_1fr] gap-x-2 gap-y-8 py-8">
+    <main className="grid w-screen grid-cols-[1fr_min(56rem,100%-1rem)_1fr] gap-x-2 gap-y-8 py-8">
       <h2 className="col-2 text-3xl font-semibold">
         NBA Lines{' '}
         {pstDate && (
